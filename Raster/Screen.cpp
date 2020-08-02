afa53757cd541ef996c381f63bf0b081d665889b
mstava <mustafaabdo468@gmail.com>
2020-08-02 21:05:42 +0200

    create new window and add logic to functions

--- Raster/Screen.cpp
+++ Raster/Screen.cpp
@@ -1,25 +1,95 @@
 #include "Screen.h"
 
-Screen::Screen()
+Screen::Screen() :
+    mWindow(nullptr),
+    mRenderer(nullptr),
+    isRunning(true)
 {
 }
 
-void Screen::initScreen()
+bool Screen::initScreen()
 {
+    // check SDL initialzation
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return false;
+    }
+
+    // create the window
+    mWindow = SDL_CreateWindow(
+        "Ping Pong",                       // title
+        SDL_WINDOWPOS_UNDEFINED,           // x position
+        SDL_WINDOWPOS_UNDEFINED,           // y position
+        WIDTH,                             // width
+        HEIGHT,                            // height
+        SDL_WINDOW_OPENGL                  // flag
+    );
+
+    // check window creating
+    if (mWindow == NULL)
+    {
+        SDL_Log("Unable to create SDL window: %s", SDL_GetError());
+        return false;
+    }
+
+    mRenderer = SDL_CreateRenderer(
+        mWindow,
+        -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+    );
+
+    if (mRenderer == NULL)
+    {
+        SDL_Log("Unable to create SDL renderer: %s", SDL_GetError());
+        return false;
+    }
+
+    return true;
 }
 
 void Screen::closeScreen()
 {
+    SDL_DestroyRenderer(mRenderer);
+    SDL_DestroyWindow(mWindow);
+    SDL_Quit();
 }
 
 void Screen::runScreen()
 {
+    while (isRunning)
+    {
+        processInput();
+        updateScreen();
+        renderScreen();
+    }
+}
+
+void Screen::processInput()
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            isRunning = false;
+            break;
+        }
+    }
 }
 
 void Screen::updateScreen()
 {
+    
 }
 
 void Screen::renderScreen()
 {
+    // set background color
+    SDL_SetRenderDrawColor(mRenderer, 80, 50, 15, 255);
+    SDL_RenderClear(mRenderer);
+
+    // present the render on the screen
+    SDL_RenderPresent(mRenderer);
 }
