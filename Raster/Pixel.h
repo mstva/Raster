d30e5c8a37d24bcb7e0fadeb6d21d5e2a3e7dcdc
mstava <mustafaabdo468@gmail.com>
2020-08-02 22:06:24 +0200

    issue#5 draw pixel on screen

---
+++ Raster/Pixel.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "SDL.h"
+#include "Constants.h"
+
+class Pixel
+{
+public:
+	// public functions
+	void plot_pixel_ndc(float ndcX, float ndcY, SDL_Renderer* mRenderer);
+
+private:
+	// private functions
+
+	// private variable
+
+	// logic varaiables
+};
+
